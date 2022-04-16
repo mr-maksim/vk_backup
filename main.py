3e68e8b06917c903aeb542af79b16f06c16df02d
mr-maksim <55948966+mr-maksim@users.noreply.github.com>
2022-04-17 02:02:00 +0300

    Added new functions: In class VkBackup: * __date_convert()
    In class YaLoader:
    * _get_headers()
    * _create_upload_folder()
    * _get_link()
    * upload()

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import config
 import requests
+import datetime
 
 
 class VkBackup:
@@ -32,6 +33,11 @@ class VkBackup:
             return sizes['width']
         else:
             return sizes['height']
+        
+    def __date_convert(self,ts):
+        date = str(datetime.datetime.fromtimestamp(ts).strftime('%d.%m.%Y'))
+        return date
+
 
     def upload_json(self):
         photo_list = []
@@ -42,7 +48,8 @@ class VkBackup:
             size = image['sizes']
             size_url = max(size, key=VkBackup.__big_party)
             if str(image['likes']['count'])+file_extension in last_name:
-                photo_list.append({'file_name': str(image['date']) + '_' + str(
+                date = self.__date_convert(image['date'])
+                photo_list.append({'file_name': date + '_' + str(
                     image['likes']['count']) + file_extension, 'size': size_url['type'], 'url': size_url['url']})
                 last_name.append(str(image['likes']['count'])+file_extension)
             elif str(image['likes']['count'])+file_extension not in last_name:
@@ -51,13 +58,47 @@ class VkBackup:
                 last_name.append(str(image['likes']['count'])+file_extension)
         with open(f'{config.JSON_PATH}/upload_json.json', 'w') as file:
             file.write(json.dumps(photo_list, indent=4))
+            return photo_list
+
 
 class YaLoader:
-    def __init__(self, ya_token,api,path):
+    def __init__(self, ya_token, api, path):
         self.token = ya_token
         self.base_url = api
         self.folder = path
 
+    def _get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': self.token
+        }
+
+    def _create_upload_folder(self):
+        href = self.base_url + 'v1/disk/resources?path=' + self.folder
+        response = requests.put(href, headers=self._get_headers())
+
+    def _get_link(self,file_path, name):
+        upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        params = {
+            'path': f'{file_path}/{name}',
+            'overwrite': 'true'
+        }
+        response = requests.get(upload_url, headers=self._get_headers(), params=params)
+        return response.json()
+
+    def upload(self,photo_list):
+            for item in photo_list:
+                dict = self._get_link(file_path=self.folder, name=item['file_name'])
+                href = dict['href']
+                image = requests.get(item['url'])
+                response = requests.put(href, data=image.content)
+                response.raise_for_status()
+                if response.status_code == 201:
+                    print('Uploaded')
+
+
 if __name__ == "__main__":
-    back = VkBackup(config.VK_TOKEN, config.API_VK)
-    back.upload_json()
+    vk_back = VkBackup(config.VK_TOKEN, config.API_VK)
+    ya_back = YaLoader(config.YA_TOKEN,config.API_YAD,config.YA_FOLDER)
+    ya_back._create_upload_folder()
+    ya_back.upload(vk_back.upload_json())
