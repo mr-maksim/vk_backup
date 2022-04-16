550aca4f0abdbd7ab2b27c8368a75490999eb0db
mr-maksim <55948966+mr-maksim@users.noreply.github.com>
2022-04-16 23:24:35 +0300

    Added class VkBackup

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-import __hello__
+class VkBackup:
+    def __init__(self, vk_token, api, count = 2) -> None:
+        self.token = vk_token
+        self.base_url = api
+        self.count = count
+
+    
