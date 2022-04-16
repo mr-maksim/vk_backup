8b410b5ea42dc8515d1c0c44a874b2b565c03724
mr-maksim <55948966+mr-maksim@users.noreply.github.com>
2022-04-16 23:37:16 +0300

    Added function photo_json_response()

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import json
+import config
+import requests
+
+
 class VkBackup:
-    def __init__(self, vk_token, api, count = 2) -> None:
+    def __init__(self, vk_token, api, count=2) -> None:
         self.token = vk_token
         self.base_url = api
         self.count = count
@@ -10,10 +15,18 @@ class VkBackup:
         params = {
             'album_id': 'profile',
             'extended': 1,
-            'count':self.count,
+            'count': self.count,
             'photo_sizes': 1,
             'access_token': self.token,
             'v': 5.131
         }
         response = requests.get(href, params=params)
         return response.json()
+
+    def photo_json_response(self):
+        with open(f'{config.JSON_PATH}/photo_json_response.json', 'w') as file:
+            file.write(json.dumps(self.__vk_response(), indent=4))
+
+
+back = VkBackup(config.VK_TOKEN, config.API_VK)
+back.photo_json_response()
