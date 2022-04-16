6b53544f9bda02b9b11af0d64a0e8f922c9774ef
mr-maksim <55948966+mr-maksim@users.noreply.github.com>
2022-04-16 23:28:12 +0300

    Added finction __vk_response()

--- main.py
+++ main.py
@@ -4,4 +4,16 @@ class VkBackup:
         self.base_url = api
         self.count = count
 
-    
+    def __vk_response(self):
+        method = 'photos.get/'
+        href = self.base_url + method
+        params = {
+            'album_id': 'profile',
+            'extended': 1,
+            'count':self.count,
+            'photo_sizes': 1,
+            'access_token': self.token,
+            'v': 5.131
+        }
+        response = requests.get(href, params=params)
+        return response.json()
