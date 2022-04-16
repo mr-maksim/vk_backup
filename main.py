a37a2eb3d26b60a42b7648e604981a66bf0d227f
mr-maksim <55948966+mr-maksim@users.noreply.github.com>
2022-04-16 23:45:40 +0300

    Added functions: * upload_json() * __big_party()

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 
 
 class VkBackup:
-    def __init__(self, vk_token, api, count=2) -> None:
+    def __init__(self, vk_token, api, count=5) -> None:
         self.token = vk_token
         self.base_url = api
         self.count = count
@@ -27,6 +27,31 @@ class VkBackup:
         with open(f'{config.JSON_PATH}/photo_json_response.json', 'w') as file:
             file.write(json.dumps(self.__vk_response(), indent=4))
 
+    def __big_party(sizes):
+        if sizes['width'] >= sizes['height']:
+            return sizes['width']
+        else:
+            return sizes['height']
+
+    def upload_json(self):
+        photo_list = []
+        last_name = []
+        response = self.__vk_response()["response"]
+        file_extension = '.jpg'
+        for image in response['items']:
+            size = image['sizes']
+            size_url = max(size, key=VkBackup.__big_party)
+            if str(image['likes']['count'])+file_extension in last_name:
+                photo_list.append({'file_name': str(image['date']) + '_' + str(
+                    image['likes']['count']) + file_extension, 'size': size_url['type'], 'url': size_url['url']})
+                last_name.append(str(image['likes']['count'])+file_extension)
+            elif str(image['likes']['count'])+file_extension not in last_name:
+                photo_list.append({'file_name': str(
+                    image['likes']['count']) + file_extension, 'size': size_url['type'], 'url': size_url['url']})
+                last_name.append(str(image['likes']['count'])+file_extension)
+        with open(f'{config.JSON_PATH}/upload_json.json', 'w') as file:
+            file.write(json.dumps(photo_list, indent=4))
+
 
 back = VkBackup(config.VK_TOKEN, config.API_VK)
-back.photo_json_response()
+back.upload_json()
