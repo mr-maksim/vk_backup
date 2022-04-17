068d7cb68becbd87afd09e91e1952167a8d65992
mr-maksim <55948966+mr-maksim@users.noreply.github.com>
2022-04-17 18:25:24 +0300

    Minor changes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import config
 import requests
 import datetime
+from tqdm import tqdm
 
 
 class VkBackup:
@@ -33,11 +34,10 @@ class VkBackup:
             return sizes['width']
         else:
             return sizes['height']
-        
-    def __date_convert(self,ts):
+
+    def __date_convert(self, ts):
         date = str(datetime.datetime.fromtimestamp(ts).strftime('%d.%m.%Y'))
         return date
-
 
     def upload_json(self):
         photo_list = []
@@ -45,17 +45,21 @@ class VkBackup:
         last_name = []
         response = self.__vk_response()["response"]
         file_extension = '.jpg'
-        for image in response['items']:
+        for image in tqdm(response['items'], desc='Получение url максимального размера'):
             size = image['sizes']
             size_url = max(size, key=VkBackup.__big_party)
             if str(image['likes']['count'])+file_extension in last_name:
                 date = self.__date_convert(image['date'])
-                photo_list.append({'file_name': date + '_' + str(image['likes']['count']) + file_extension, 'size': size_url['type'], 'url': size_url['url']})
-                save_json.append({'file_name': date + '_' + str(image['likes']['count']) + file_extension, 'size': size_url['type']})
+                photo_list.append({'file_name': date + '_' + str(
+                    image['likes']['count']) + file_extension, 'size': size_url['type'], 'url': size_url['url']})
+                save_json.append({'file_name': date + '_' + str(
+                    image['likes']['count']) + file_extension, 'size': size_url['type']})
                 last_name.append(str(image['likes']['count'])+file_extension)
             elif str(image['likes']['count'])+file_extension not in last_name:
-                photo_list.append({'file_name': str(image['likes']['count']) + file_extension, 'size': size_url['type'], 'url': size_url['url']})
-                save_json.append({'file_name': str(image['likes']['count']) + file_extension, 'size': size_url['type']})
+                photo_list.append({'file_name': str(
+                    image['likes']['count']) + file_extension, 'size': size_url['type'], 'url': size_url['url']})
+                save_json.append({'file_name': str(
+                    image['likes']['count']) + file_extension, 'size': size_url['type']})
                 last_name.append(str(image['likes']['count'])+file_extension)
         with open(f'{config.JSON_PATH}/upload_json.json', 'w') as file:
             file.write(json.dumps(save_json, indent=4))
@@ -78,28 +82,29 @@ class YaLoader:
         href = self.base_url + 'v1/disk/resources?path=' + self.folder
         response = requests.put(href, headers=self._get_headers())
 
-    def _get_link(self,file_path, name):
+    def _get_link(self, file_path, name):
         upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
         params = {
             'path': f'{file_path}/{name}',
             'overwrite': 'true'
         }
-        response = requests.get(upload_url, headers=self._get_headers(), params=params)
+        response = requests.get(
+            upload_url, headers=self._get_headers(), params=params)
         return response.json()
 
-    def upload(self,photo_list):
-            for item in photo_list:
-                dict = self._get_link(file_path=self.folder, name=item['file_name'])
-                href = dict['href']
-                image = requests.get(item['url'])
-                response = requests.put(href, data=image.content)
-                response.raise_for_status()
-                if response.status_code == 201:
-                    print('Uploaded')
+    def upload(self, photo_list):
+        for item in tqdm(photo_list, desc='Загрузка на Ядиск'):
+            dict = self._get_link(file_path=self.folder,
+                                  name=item['file_name'])
+            href = dict['href']
+            image = requests.get(item['url'])
+            response = requests.put(href, data=image.content)
+            response.raise_for_status()
 
 
 if __name__ == "__main__":
     vk_back = VkBackup(config.VK_TOKEN, config.API_VK)
-    ya_back = YaLoader(config.YA_TOKEN,config.API_YAD,config.YA_FOLDER)
+    ya_back = YaLoader(config.YA_TOKEN, config.API_YAD, config.YA_FOLDER)
     ya_back._create_upload_folder()
     ya_back.upload(vk_back.upload_json())
+    print('Работа завершена')
