d858ff38623e9e24051af4fa17894b8d9892a53b
mr-maksim <55948966+mr-maksim@users.noreply.github.com>
2022-04-24 11:38:44 +0300

    Minor changes

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from modules.ya_class import YaLoader
 
 
 if __name__ == "__main__":
-    vk_back = VkBackup(config.VK_TOKEN, config.API_VK)
+    screen_name = input('Введите Screen Name или ID: ')
+    count = int(input('Введите количество загружаемых фото: '))
+    vk_back = VkBackup(screen_name, config.VK_TOKEN,
+                       config.API_VK, count=count)
     ya_back = YaLoader(config.YA_TOKEN, config.API_YAD, config.YA_FOLDER)
     ya_back._create_upload_folder()
     ya_back.upload(vk_back.upload_json())
